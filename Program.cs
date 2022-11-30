6139155b9ce1b70c9d1a8198c40e07b73e7c7951
baltov3 <116825337+baltov3@users.noreply.github.com>
2022-11-30 12:09:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] name = Console.ReadLine().Split();
+            Queue<string> queue = new Queue<string>(name);
+            int tosscountToRemove = int.Parse(Console.ReadLine());
+            int currentTosses = 1;
+            while (queue.Count > 1)
+            {
+                var kindWithPateto = queue.Dequeue();
+                if (currentTosses != tosscountToRemove)
+                {
+                    currentTosses = 1;
+                    queue.Enqueue(kindWithPateto);
+
+                }
+                else
+                {
+                    Console.WriteLine($"Removed {kindWithPateto}");
+                    currentTosses++;
+                }
+
+            }
+            Console.WriteLine($"Lasr is {queue.Dequeue()}");
+        }
+    }
+}
