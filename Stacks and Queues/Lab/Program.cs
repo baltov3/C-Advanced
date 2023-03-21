1eeca0ad07863dd2e35a20ba007ced4ada1f163a
baltov3 <116825337+baltov3@users.noreply.github.com>
2023-03-21 10:03:43 +0200

    Add files via upload

---
+++ Stacks and Queues/Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Stack<char> stack = new Stack<char>();
+            foreach (var item in a)
+            {
+                stack.Push(item);
+            }
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
