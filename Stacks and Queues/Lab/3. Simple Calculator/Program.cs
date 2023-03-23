76eacb4484230cf5a971995e0bf15f5f00e30cab
baltov3 <116825337+baltov3@users.noreply.github.com>
2023-03-23 02:09:38 +0200

    Add files via upload

---
+++ Stacks and Queues/Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            Stack<string> stack = new Stack<string>(input.Reverse());
+            while (stack.Count > 1)
+            {
+                int firstnum = int.Parse(stack.Pop());
+                string action = stack.Pop();
+                int secondnum = int.Parse(stack.Pop());
+                if (action == "+")
+                {
+                    stack.Push((firstnum + secondnum).ToString());
+                }
+                if (action == "-")
+                {
+                    stack.Push((firstnum - secondnum).ToString());
+                }
+            }
+            Console.WriteLine(stack.Peek());
+
+
+        }
+    }
+}
