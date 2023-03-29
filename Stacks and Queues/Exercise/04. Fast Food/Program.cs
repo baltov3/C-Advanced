148c9302c99799eba47557bbf1ac92feff528430
baltov3 <116825337+baltov3@users.noreply.github.com>
2023-03-29 09:40:37 +0300

    Add files via upload

---
+++ Stacks and Queues/Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jagged
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalQuantity = int.Parse(Console.ReadLine());
+            int[] inputOrders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(inputOrders);
+            Console.WriteLine(inputOrders.Max());
+            while (queue.Count > 0 && totalQuantity >= 0)
+            {
+                int currentOrder = queue.Peek();
+                if (totalQuantity - currentOrder < 0)
+                {
+                    break;
+                }
+
+                totalQuantity -= currentOrder;
+                queue.Dequeue();
+
+            }
+            if (queue.Count > 0)
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", queue)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+
+
+        }
+    }
+}
