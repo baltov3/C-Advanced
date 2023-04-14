a33fe8772f16e5fb100607c8b89c3694bff0b311
baltov3 <116825337+baltov3@users.noreply.github.com>
2023-04-14 12:26:05 +0300

    Add files via upload

---
+++ Streams, Files and Directories/Exercises/02. Line Numbers/LineNumbers.cs
@@ -0,0 +1,31 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            string[] lines = File.ReadAllLines(inputFilePath);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int letters = lines[i].Count(x => char.IsLetter(x));
+                int marks = lines[i].Count(x => char.IsPunctuation(x));
+                stringBuilder.AppendLine($"Line {i + 1}: -{lines[i]} ({letters})({marks})");
+            }
+            File.WriteAllText(outputFilePath, stringBuilder.ToString().TrimEnd());
+        }
+    }
+}
