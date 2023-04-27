eabea1dcfe7a2f8df3db4b13007efe6993ff1ebd
baltov3 <116825337+baltov3@users.noreply.github.com>
2023-04-27 10:37:46 +0300

    Add files via upload

---
+++ Streams, Files and Directories/Exercises/03. Copy Binary File/CopyBinaryFile.cs
@@ -0,0 +1,32 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            FileStream fileStreamReader = new FileStream(inputFilePath, FileMode.Open);
+            FileStream fileStreamWriter = new FileStream(outputFilePath, FileMode.Create);
+            byte[] bytes = new byte[256];
+            while (true)
+            {
+                int currentbutes = fileStreamReader.Read(bytes, 0, bytes.Length);
+                if (currentbutes==0)
+                {
+                    break;
+                }
+                fileStreamWriter.Write(bytes, 0, bytes.Length);
+            }
+        }
+    }
+}
