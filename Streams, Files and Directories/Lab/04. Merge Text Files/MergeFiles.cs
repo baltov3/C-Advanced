1cd27c236634540f81a6c4c4a79748623bd88f72
baltov3 <116825337+baltov3@users.noreply.github.com>
2023-05-04 02:23:47 +0300

    Add files via upload

---
+++ Streams, Files and Directories/Lab/04. Merge Text Files/MergeFiles.cs
@@ -0,0 +1,52 @@
+﻿namespace MergeFiles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            StreamReader reader1 = new StreamReader(firstInputFilePath);
+            StreamReader reader2 = new StreamReader(secondInputFilePath);
+            SortedSet<int> numbers = new SortedSet<int>();
+            while (true)
+            {
+              string num= reader1.ReadLine();
+                if (num==null)
+                {
+                    break;
+                }
+                int numeber = int.Parse(num);
+                numbers.Add(numeber);
+                
+            }
+            while (true)
+            {
+                string num = reader2.ReadLine();
+                if (num == null)
+                {
+                    break;
+                }
+                int numeber = int.Parse(num);
+                numbers.Add(numeber);
+
+            }
+            StreamWriter streamWriter = new StreamWriter(outputFilePath);
+            foreach (var item in numbers)
+            {
+                streamWriter.WriteLine(item);
+            }
+
+        }
+    }
+}
