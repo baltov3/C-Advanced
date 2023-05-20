6783e6973acc18addd27f5d0658cd2da2256b986
baltov3 <116825337+baltov3@users.noreply.github.com>
2023-05-20 14:54:36 +0300

    Add files via upload

---
+++ Defining Classes /Lab/02. Car Extension/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+     public class Car
+    {
+         private string make;
+         private string model;
+        private int year;
+        private double fuelConsumption;
+        private double fuelQuantity;
+        public int Year  { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+      public void Drive(double distance)
+        {
+           fuelQuantity = fuelQuantity-distance;
+            if (fuelQuantity < 0)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+       public string WhoAmI()
+        {
+            return $"Make: {this.Make} " +
+                $"Model: {this.Model}" +
+                $"Year: {this.Year}" +
+                $"Fuel: {this.FuelQuantity:F2}";
+        }
+    }
+}
